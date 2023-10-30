9b2a0779c5fc1b9b23e373f2e15027286dd370c4
igor-srajer <igorsrajer123@gmail.com>
2023-10-30 22:45:13 +0100

    Create animated parallax for moon and sun

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./app.scss";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Sidebar from "./components/sidebar/Sidebar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return (
     <div>
       <section id="Homepage">
         <Navbar />
-        <Sidebar />
+        <Hero />
       </section>
-      <section id="Services">Parallax</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
       <section id="Portfolio">Services</section>
-      <section id="Contact">Parallax</section>
+      <section id="Contact">
+        <Parallax type="portfolio" />
+      </section>
       <section id="About">Portfolio1</section>
       <section>Contact</section>
     </div>
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,79 @@
+import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-200%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
+    },
+  },
+};
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>IGOR SRAJER</motion.h2>
+          <motion.h1 variants={textVariants}>Busitelj rupa</motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button>See the Latest Work</motion.button>
+            <motion.button>Contact Me!</motion.button>
+          </motion.div>
+          <motion.img
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+            variants={textVariants}
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Gospodar svemira i univerzuma bree
+      </motion.div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
